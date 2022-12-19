4d353c1dc3c697193f5cc9f26b21daf361fff5ee
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2022-12-20 00:50:24 +0100

    Update JokeServiceImpl.java

--- src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class JokeServiceImpl implements JokeService{
 
-    private final ChuckNorrisQuotes chuckNorrisQuotes;
+    private final ChuckNorrisQuotes chuckNorrisQuotes; //get bean from xml configuration
 
     public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
         this.chuckNorrisQuotes = chuckNorrisQuotes;
